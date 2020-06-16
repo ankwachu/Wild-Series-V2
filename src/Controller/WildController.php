a6aa6a48308c33fd9fc133c8d19136ff80cce1d5
Anthony Kwan Chung <anthonykwanchung@gmail.com>
2020-06-16 20:45:52 +0200

    show episode by id & display message no season and no episode if not found

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Program;
 use App\Entity\Category;
 use App\Entity\Season;
+use App\Entity\Episode;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -149,6 +150,22 @@ class WildController extends AbstractController
             'season'   => $season,
             'program'  => $program,
             'episodes' => $episodes,
+        ]);
+    }
+
+    /**
+     * @Route("/episode/{id}",
+     *     name="episode")
+     */
+    public function showEpisode(Episode $episode): Response
+    {
+        $season = $episode->getSeasons();
+        $program = $season->getProgram();
+
+        return $this->render('wild/episode.html.twig', [
+            'episode' => $episode,
+            'season'   => $season,
+            'program'  => $program,
         ]);
     }
 }
