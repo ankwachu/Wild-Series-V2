e66831ee939b0c09c21e5d51ccacd964491444ca
Anthony Kwan Chung <anthonykwanchung@gmail.com>
2020-07-28 18:43:59 +0200

    User can add program to favorite

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,25 +8,32 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\EpisodeRepository;
 use App\Repository\ProgramRepository;
 use App\Entity\Program;
+use App\Repository\CommentRepository;
+
 class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
-    */
+     */
 
-    public function homepage(EpisodeRepository $episodeRepository) :Response
-    {        
+    public function homepage(EpisodeRepository $episodeRepository, CommentRepository $commentRepository): Response
+    {
         $programs = $this->getDoctrine()
             ->getRepository(Program::class)
-            ->findBy([],
-                    ['id' => 'DESC'],
-                    3
+            ->findBy(
+                [],
+                ['id' => 'DESC'],
+                3
             );
 
-        
-        return $this->render ('homepage/index.html.twig', [
+        return $this->render('homepage/index.html.twig', [
             'programs' => $programs,
             'episodes' => $episodeRepository->findByDateExpiration(),
+            'comments' => $commentRepository->findBy(
+                [],
+                ['publishedAt' => 'DESC'],
+                4
+            ),
         ]);
     }
 }
